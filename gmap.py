5de7a2dc4fc2474b8b56613592bdb989e8965f96
Russell Chen <rrruss.t.y@gmail.com>
2021-04-09 23:06:53 -0700

    first commit

---
+++ gmap.py
@@ -0,0 +1,98 @@
+import praw
+from praw.util.token_manager import FileTokenManager
+from processing import handle_submission
+import argparse
+import logging
+
+
+REFRESH_TOKEN_FILENAME = "refresh_token.txt"  # has to be uploaded
+
+
+CROSSPOST_TO = "picsmirrortest"
+SOURCE_SUBS = ["EarthPorn"]
+
+
+def main(
+        read_mode: str,
+        limit: int,
+        crosspost: bool,
+        pause_after: int
+    ) -> None:
+    reddit = praw.Reddit(
+        token_manager=FileTokenManager(REFRESH_TOKEN_FILENAME),
+        user_agent="use_file_token_manager/v0 by u/bboe",
+        check_for_async=False
+    )
+
+    scopes = reddit.auth.scopes()
+    if scopes == {"*"}:
+        print(f"{reddit.user.me()} is authenticated with all scopes")
+    elif "identity" in scopes:
+        print(f"{reddit.user.me()} is authenticated with the following scopes: {scopes}")
+    else:
+        print(f"You are authenticated with the following scopes: {scopes}")
+
+    print(f"Read-only is {reddit.read_only}")
+
+    sub = reddit.subreddit('+'.join(SOURCE_SUBS))  # + to specify multiple subreddits
+
+    if read_mode == 'new':
+        for submission in sub.new(limit=limit):
+            handle_submission(submission, crosspost=crosspost, crosspost_to=CROSSPOST_TO)
+    elif read_mode == 'hot':
+        for submission in sub.hot(limit=limit):
+            handle_submission(submission, crosspost=crosspost, crosspost_to=CROSSPOST_TO)
+    elif read_mode == 'rising':
+        for submission in sub.rising(limit=limit):
+            handle_submission(submission, crosspost=crosspost, crosspost_to=CROSSPOST_TO)
+    elif read_mode == 'stream':
+        posts_read = 0
+        for submission in sub.stream.submissions(skip_existing=True, pause_after=pause_after):
+            if submission is None:
+                print(f"Stream paused after {pause_after} requests.")
+                break
+            
+            handle_submission(submission, crosspost=crosspost, crosspost_to=CROSSPOST_TO)
+
+            posts_read += 1
+            if posts_read >= limit:
+                break
+
+    return None
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Read reddit titles and infer location.")
+    parser.add_argument('--mode',
+                        dest='mode',
+                        required=True,
+                        choices=['new', 'hot', 'stream', 'rising'],
+                        default='new',
+                        help='Mode of reading subreddit: stream, new, etc.')
+    parser.add_argument('--limit',
+                        dest='limit',
+                        required=False,
+                        default=2,
+                        type=int,
+                        help='Number of posts to read.')
+    parser.add_argument('--crosspost',
+                        dest='crosspost',
+                        action='store_true',
+                        help='Crosspost to a subreddit and reply to that crosspost.')
+    parser.add_argument('--no-crosspost',
+                        dest='crosspost',
+                        action='store_false',
+                        help='Read posts without crossposting.')
+    parser.set_defaults(crosspost=False)
+    parse.add_argument('--pauseafter',
+                       dest='pauseafter',
+                       required=False,
+                       help='Number of stream requests yielding no new items before pause. Never pauses if not specified.')
+    args = parser.parse_args()
+    main(
+        read_mode=args.mode,
+        limit=max(1, args.limit),
+        crosspost=args.crosspost,
+        pause_after=args.pauseafter
+    )
---
+++ processing.py
@@ -0,0 +1,92 @@
+from praw.exceptions import RedditAPIException
+from praw.models.reddit.submission import Submission
+from praw.models.reddit.comment import Comment
+import re
+import spacy
+from urllib.parse import quote_plus
+import functools
+import time
+import logging
+
+nlp = spacy.load("en_core_web_trf")
+
+
+def clean_title(title: str) -> str:
+    title = re.sub(r'(\(|\[)OC(\)|\])', '', title)  # removes (OC), [OC]
+    title = re.sub(r'(\(|\[)?[0-9]+ ?[x|X|×] ?[0-9]+(\)|\])?', '', title)  # removes [4000x3000], (4000x3000) etc
+    title = re.sub(r'@( )?[a-zA-Z_0-9.]+', '', title)  # removes @ social media handles
+    title = title.strip()
+
+    return title
+
+
+def get_location(title: str) -> str:
+    doc = nlp(title)
+    loc, gpe = [], []
+
+    for entity in doc.ents:
+        print(entity.text, entity.label_)
+        if entity.label_ == 'LOC' and entity.text not in loc:  # very occasionally there are duplicates in a long title
+            loc.append(entity.text)
+        elif entity.label_ == 'GPE' and entity.text not in gpe:
+            gpe.append(entity.text)
+    
+    location = loc + gpe  # loc is more specific than gpe so loc should come first
+
+    return ', '.join(location)
+
+
+def get_gmap_link(location: str, prefix: str = 'https://www.google.com/maps/search/?api=1&query=') -> str:
+    # this function can probably be made more useful when we want to use other google map APIs
+    return prefix + quote_plus(location)
+
+
+def retry(retry_count=5, delay=60, allowed_exceptions=(RedditAPIException)):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            for _ in range(retry_count):
+                try:
+                    result = func(*args, **kwargs)
+                    if result:
+                        return result
+                except allowed_exceptions as e:
+                    time.sleep(delay)
+                    pass
+        return wrapper
+    return decorator
+
+
+@retry(retry_count=5, delay=2*60)  # I haven't been rate limited when cross posting so I don't think this delay needs to be very long
+def crosspost_to_sub(submission: Submission, **kwargs) -> Submission:
+    cross_post = submission.crosspost(**kwargs)
+    return cross_post
+
+
+@retry(retry_count=3, delay=10*60)  # rate limit (at least on r/test) seems to be 7++
+def comment_on_post(submission: Submission, commenttext: str) -> Comment:
+    comment = submission.reply(commenttext)
+    return comment
+
+
+def handle_submission(submission: Submission, crosspost: bool, crosspost_to: str) -> None:    
+    print(f"submission id {submission} has title {submission.title}, photo can be found at {submission.url}")
+    title = clean_title(submission.title)
+    print(f"Cleaner title: {title}")
+    
+    location = get_location(title)
+
+    if location:
+        print(f"Location is {location}")
+
+        gmapsearchlink = get_gmap_link(location)
+        print(gmapsearchlink)
+
+        if crosspost:
+            cross_post = crosspost_to_sub(submission=submission, subreddit=crosspost_to, title=location, send_replies=False)
+            comment_on_post(submission=cross_post, commenttext=gmapsearchlink)
+        
+    else:
+        print("No location found\n")
+
+    return None
